6abba7723d6433dc6aee3d98f806b51c728abab1
CaueSF2 <cauesf0726@gmail.com>
2023-01-28 14:41:45 -0300

    posicionando frota

--- EP2.py
+++ EP2.py
@@ -91,3 +91,76 @@ def posicao_valida(info_frota, linha, coluna, orientacao, tamanho):
                     if linha3 == linha2 and coluna3 == coluna2:
                         return False
     return True
+
+#POSICIONANDO FROTA
+frota = {
+    "porta-aviões":[],
+    "navio-tanque":[],
+    "contratorpedeiro":[],
+    "submarino": [],
+}
+
+i = 0
+while i < 1:
+    print('Insira as informações referentes ao navio porta-aviões que possui tamanho 4')
+    q_linha = int(input('Qual linha? '))
+    q_coluna = int(input('Qual coluna? '))
+    q_orientacao = int(input(('[1] Vertical [2] Horizontal >')))
+    if q_orientacao == 1:
+        q_orientacao = 'vertical'
+    elif q_orientacao == 2:
+        q_orientacao = 'horizontal'
+
+    if posicao_valida(frota, q_linha, q_coluna, q_orientacao, 4) == True:
+        preenche_frota(frota, 'porta-aviões', q_linha, q_coluna, q_orientacao, 4)
+        i += 1
+    else:
+        print('Esta posição não está válida!')
+
+i = 0
+while i < 2:
+    print('Insira as informações referentes ao navio navio-tanque que possui tamanho 3')
+    q_linha = int(input('Qual linha? '))
+    q_coluna = int(input('Qual coluna? '))
+    q_orientacao = int(input(('[1] Vertical [2] Horizontal >')))
+    if q_orientacao == 1:
+        q_orientacao = 'vertical'
+    elif q_orientacao == 2:
+        q_orientacao = 'horizontal'
+
+    if posicao_valida(frota, q_linha, q_coluna, q_orientacao, 3) == True:
+        preenche_frota(frota, 'navio-tanque', q_linha, q_coluna, q_orientacao, 3)
+        i += 1
+    else:
+        print('Esta posição não está válida!')
+
+i = 0
+while i < 3:
+    print('Insira as informações referentes ao navio contratorpedeiro que possui tamanho 2')
+    q_linha = int(input('Qual linha? '))
+    q_coluna = int(input('Qual coluna? '))
+    q_orientacao = int(input(('[1] Vertical [2] Horizontal >')))
+    if q_orientacao == 1:
+        q_orientacao = 'vertical'
+    elif q_orientacao == 2:
+        q_orientacao = 'horizontal'
+
+    if posicao_valida(frota, q_linha, q_coluna, q_orientacao, 2) == True:
+        preenche_frota(frota, 'contratorpedeiro', q_linha, q_coluna, q_orientacao, 2)
+        i += 1
+    else:
+        print('Esta posição não está válida!')
+
+i = 0
+while i < 4:
+    print('Insira as informações referentes ao navio submarino que possui tamanho 1')
+    q_linha = int(input('Qual linha? '))
+    q_coluna = int(input('Qual coluna? '))
+
+    if posicao_valida(frota, q_linha, q_coluna, q_orientacao, 1) == True:
+        preenche_frota(frota, 'submarino', q_linha, q_coluna, q_orientacao, 1)
+        i += 1
+    else:
+        print('Esta posição não está válida!')
+
+print(frota)
